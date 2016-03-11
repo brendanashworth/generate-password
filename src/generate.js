19fbb9d99efdf539cee5f5a0992c55d7806c813b
Algimantas Krasauskas <algimantas.krasauskas@dominionmarinemedia.com>
2016-03-11 15:22:02 +0000

    Should fix unix indentation issue

--- src/generate.js
+++ src/generate.js
@@ -4,8 +4,8 @@ var self = module.exports;
 
 // Generates a random number
 var randomNumber = function(max) {
-	// gives a number between 0 (inclusive) and max (exclusive)
-	return crypto.randomBytes(1)[0] % max;
+  // gives a number between 0 (inclusive) and max (exclusive)
+  return crypto.randomBytes(1)[0] % max;
 };
 
 // Possible combinations
@@ -17,37 +17,37 @@ var lowercase = 'abcdefghijklmnopqrstuvwxyz',
 
 // Generate a random password.
 self.generate = function(options) {
-	// Set defaults.
-	if (!options.hasOwnProperty('length')) options.length = 10;
-	if (!options.hasOwnProperty('numbers')) options.numbers = false;
-	if (!options.hasOwnProperty('symbols')) options.symbols = false;
-	if (!options.hasOwnProperty('uppercase')) options.uppercase = true;
-	if (!options.hasOwnProperty('excludeSimilarCharacters')) options.excludeSimilarCharacters = false;
-	if (!options.hasOwnProperty('strict')) options.strict = false;
+  // Set defaults.
+  if (!options.hasOwnProperty('length')) options.length = 10;
+  if (!options.hasOwnProperty('numbers')) options.numbers = false;
+  if (!options.hasOwnProperty('symbols')) options.symbols = false;
+  if (!options.hasOwnProperty('uppercase')) options.uppercase = true;
+  if (!options.hasOwnProperty('excludeSimilarCharacters')) options.excludeSimilarCharacters = false;
+  if (!options.hasOwnProperty('strict')) options.strict = false;
 
-	// Generate character pool
-	var pool = lowercase;
+  // Generate character pool
+  var pool = lowercase;
 
-	// uppercase
-	if (options.uppercase) {
-		pool += uppercase;
-	}
-	// numbers
-	if (options.numbers) {
-		pool += numbers;
-	}
-	// symbols
-	if (options.symbols) {
-		pool += symbols;
-	}
-	// similar characters
-	if (options.excludeSimilarCharacters) {
-		pool = pool.replace(similarCharacters, '');
-	}
+  // uppercase
+  if (options.uppercase) {
+    pool += uppercase;
+  }
+  // numbers
+  if (options.numbers) {
+    pool += numbers;
+  }
+  // symbols
+  if (options.symbols) {
+    pool += symbols;
+  }
+  // similar characters
+  if (options.excludeSimilarCharacters) {
+    pool = pool.replace(similarCharacters, '');
+  }
 
-	var password = '';
+  var password = '';
 
-	if(options.strict){
+  if(options.strict){
     var minStrictLength = (options.numbers ? 1 : 0) + (options.symbols ? 1 : 0) + (options.uppercase ? 1 : 0);
     if(minStrictLength > options.length){
       throw(new Error('Length should correlate with strict guidelines'));
@@ -73,22 +73,22 @@ self.generate = function(options) {
       password = strictGroup.join('');
     }
 
-	} else {
-		for (var i = 0; i < options.length; i++) {
-			password += pool[randomNumber(pool.length)];
-		}
-	}
+  } else {
+    for (var i = 0; i < options.length; i++) {
+      password += pool[randomNumber(pool.length)];
+    }
+  }
 
-	return password;
+  return password;
 };
 
 // Generates multiple passwords at once with the same options.
 self.generateMultiple = function(amount, options) {
-	var passwords = [];
+  var passwords = [];
 
-	for (var i = 0; i < amount; i++) {
-		passwords[i] = self.generate(options);
-	}
+  for (var i = 0; i < amount; i++) {
+    passwords[i] = self.generate(options);
+  }
 
-	return passwords;
+  return passwords;
 };
